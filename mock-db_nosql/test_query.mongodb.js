198c1c0d976bf4b20e7b0dd49dc4c8651b822d49
ph <richtleboy@gmail.com>
2025-10-29 11:47:26 +0700

    feat: testing

---
+++ mock-db_nosql/test_query.mongodb.js
@@ -0,0 +1,43 @@
+// use("sample_mflix");
+
+use("chrome-burger-db")
+
+// db.staff.find();
+
+// db.menu_items.find({category:"Burger"});
+
+// db.menu_items.find({}, { name: 1, price: 1, _id: 0 }).sort({ price: -1 });
+
+// db.menu_items.find({}, { name: 1, price: 1, _id: 0 }).sort({ price: 1 }).limit(3);
+
+//  db.staff.findOne({first_name: "Jane", last_name: "Doe"});
+
+
+db.menu_items.aggregate([
+    {
+        $match: { name: "Bacon Cheeseburger" }
+    // },
+    // {
+    //     $unwind: "$recipe"
+    // },
+    // {
+    //     $lookup: {
+    //         from: "ingredients",
+    //         localField: "recipe.ingredient_id",
+    //         foreignField: "_id",
+    //         as: "ingredient_detail"
+    //     }
+    // },
+    // {
+    //     $unwind: "$ingredient_detail"
+    },
+    {
+        $project: {
+            _id: 0,
+            name: "$recipe.name",
+            quantity_needed: "$recipe.quantity_needed",
+            unit: "$ingredient_detail.unit"
+        }
+    }
+]);
+
